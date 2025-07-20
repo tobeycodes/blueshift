ab62bc0452f55fc0f09c025dbc98254e0e7d9ef0
Tobey Codes <tobey@tobey.codes>
2025-07-20 11:45:59 -0600

    feat: add anchor escrow

---
+++ anchor-escrow/programs/blueshift_anchor_escrow/src/errors.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum EscrowError {
+    #[msg("Invalid amount")]
+    InvalidAmount,
+
+    #[msg("Invalid maker")]
+    InvalidMaker,
+
+    #[msg("Invalid mint a")]
+    InvalidMintA,
+
+    #[msg("Invalid mint b")]
+    InvalidMintB,
+}
